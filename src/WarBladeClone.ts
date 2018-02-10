fafd5c5196e2db233cd74e8cdff3bc5fc09c5449
alaska321 <mario.griese@yahoo.de>
2018-02-10 17:28:16 +0100

    add tutorial assets

--- src/WarBladeClone.ts
+++ src/WarBladeClone.ts
@@ -3,7 +3,7 @@ export class WarBladeClone {
 
     constructor() {
         this.game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.CANVAS, 'content', 
-        { preload: this.preload, create: this.create });
+        { preload: this.preload, create: this.create, update: this.update });
     }
 
     scaleRatio: number = window.devicePixelRatio / 3;
@@ -16,8 +16,11 @@ export class WarBladeClone {
 
     create() {
         var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
-        
+        logo.anchor.setTo(0.5, 0.5); 
+    }
+
+    update(){
+
     }
 
 }
